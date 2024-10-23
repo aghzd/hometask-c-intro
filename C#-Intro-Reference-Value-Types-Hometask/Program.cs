644c7ae54d59a143a0a67767ad7aa26cbee1f6ef
aghzd <alizaminma@code.edu.az>
2024-10-23 13:55:32 +0400

    hometask c# intro

---
+++ C#-Intro-Reference-Value-Types-Hometask/Program.cs
@@ -0,0 +1,185 @@
+﻿using System.ComponentModel.Design;
+
+namespace C__Intro_Reference_Value_Types_Hometask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Verilmiş yazıdan içində boşluqlar olmayan yeni bir yazı düzəltmək
+            //string word = "  letter   ";
+            //string space = "";
+            //for (int i = 0; i < word.Length; i++)
+            //{
+            //    if (word[i] != ' ')
+            //    {
+            //        space += word[i];
+            //    }
+            //}
+            //Console.WriteLine($"yeni soz {space} dir ");
+
+
+            //2.Verilmiş yazılar siyahısında verilmiş hərfdən neçə ədəd olduğunu tapan proqram
+            //string word = "classroom";
+            //Console.WriteLine("Herf daxil edin");
+            //char  letter = char.Parse(Console.ReadLine());
+            //int count = 0;
+            //for (int i = 0; i < word.Length; i++)
+            //{
+            //    if (word[i] == letter)
+
+            //        count++;   
+            //}
+            //Console.WriteLine($"verilmis herf sozde {count} defe islenmisdir");
+
+
+
+
+            //3.Verilmiş yazının əvvəlindəki boşluqlar silinmiş bir string düzəldən proqram
+            //string word = "    salam necesen    ";
+            //string word2 = "";
+            //for (int i = 0; i < word.Length; i++)
+            //{
+            //    if (word[i] != ' ')
+            //    {
+            //        word2 += word[i];
+            //    }
+
+            //}
+            //Console.WriteLine($" yeni soz {word2} dir");
+
+
+
+            //4.Verilmiş 2 ədəd üzərində verilmiş simvola uyğun əməliyyat edən proqram.
+            //int a = 12;
+            //int b = 7;
+            //Console.WriteLine("simvolu daxil edin");
+            //char symbol = char.Parse(Console.ReadLine());
+            //int  result;
+            //switch (symbol)
+            //{
+            //    case '+':
+            //        result = a + b;
+            //        Console.WriteLine($"verilen simvol + olarsa netice = {result}");
+            //        break;
+            //    case '-':
+            //        result = a - b;
+            //        Console.WriteLine($"verilen simvol - olarsa netice = {result}");
+            //        break;
+            //    case '/':
+            //        result = a / b;
+            //        Console.WriteLine($"verilen simvol / olarsa netice = {result}");
+            //        break;
+            //    case '*':
+            //        result = a * b;
+            //        Console.WriteLine($"verilen simvol * olarsa netice = {result}");
+            //        break;
+            //    default:
+            //        Console.WriteLine("duzgun simvol daxil edin");
+            //        break;
+            //}
+
+
+
+
+
+            //5.Verilmiş yazının içində A hərfinin olub olmadığını tapan proqram
+            //string word = "zaqatala";
+            //int count = 0;
+            //for (int i = 0; i < word.Length; i++)
+            //{
+            //    if (word[i] == 'z')
+            //        count += i;
+            //}
+            //if (count != 0)
+            //    Console.WriteLine("ededde a herfi var");
+            //else
+            //    Console.WriteLine("ededde a herfi yoxdur");
+
+
+
+            //6.Verilmiş yazıda neçə A hərfinin olduğunu tapan proqram
+            //string word = "zaqatala";
+            //int count = 0;
+            //for (int i = 0; i < word.Length; i++)
+            //{
+            //    if (word[i] == 'a')
+            //        count ++;
+            //}
+            //if (count != 0)
+            //    Console.WriteLine($"ededde {count} sayda a herfi var");
+            //else
+            //    Console.WriteLine("ededde a herfi yoxdur");
+
+
+
+
+            //7.Verilmiş müsbət cüt ədədi kradtarına yüksəldən proqram. Daxil edilən ədə musbət və cüt olmadıqca yenidən daxil edilməlidir
+            //int a = 2;
+            //int sum = 1;
+            //if (a%2 == 0 && a>0)
+            //{
+            //    sum = a * a;
+            //    Console.WriteLine(sum);
+            //}
+            //else
+            //    Console.WriteLine("duzgun eded daxil edin");
+
+
+
+
+            //8.
+            //string sort = "programming";
+            //switch (sort) 
+            //{
+            //    case  "programming":
+            //        Console.WriteLine("400 saat");
+            //        break;
+            //    case "design":
+            //        Console.WriteLine("250 saat");
+            //        break;
+            //    case "system":
+            //        Console.WriteLine("200 saat");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Tehsil novu yalnisdir");
+            //        break;
+
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
